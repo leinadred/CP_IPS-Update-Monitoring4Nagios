4aba7c8842dc2941d79f45857166b62fab126e0e
Daniel Meier <dme@dmelab.local>
2019-06-17 23:34:17 +0200

    modified reference ro cp sdk (cpapi) and raise´s

--- monitor.py
+++ monitor.py
@@ -3,7 +3,7 @@
 # version 1.0
 #
 # written by Daniel Meier (github.com/leinadred)
-# July 2018
+# July 2018 - updated references to recent cpapi June 2019
 #### Handle with care ####
 #### 
 #### Changed in SDK (to unsecure - no warnings)
@@ -34,7 +34,7 @@ CRITICAL = 2
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # lib is a library that handles the communication with the Check Point management server.
-from lib import APIClient, APIClientArgs
+from cpapi import APIClient, APIClientArgs
 
 
 def main():
@@ -44,14 +44,14 @@ def main():
         #### This i
         if client.check_fingerprint() is False:
             print("Could not get the server's fingerprint - Check connectivity with the server.")
-            raise SystemExit, UNKNOWN
+            raise SystemExit("UNKNOWN")
 
         # login to server:
         login_res = client.login(args.api_user, args.api_pwd)
 
         if login_res.success is False:
             print("Login failed: {}".format(login_res.error_message))
-            raise SystemExit, UNKNOWN
+            raise SystemExit("UNKNOWN")
         monitor_ips_version = client.api_call("show-ips-status")
         if monitor_ips_version.success:
             #Editing around Vars
@@ -76,20 +76,20 @@ def main():
             if not ips_bool_update:
                 if ips_update_date_delta.days < 0:
                     print("OK! No Update available - Last installed update: {0} - Installed Version {1}" .format(ips_date_last_install_iso, ips_current_ver_info))
-                    raise SystemExit, OK
+                    raise SystemExit("OK")
                 else:
                     print("OK! No Update available - Last installed update: {0} - Installed Version {1} - Update Date Delta: {2}" .format(ips_date_last_install_iso, ips_current_ver_info, ips_update_date_delta))
-                    raise SystemExit, OK
+                    raise SystemExit("OK")
             elif ips_update_date_delta.days > 3:
                 print("CRITICAL! Updates available -  Last installed update: {0} - last Installed {1}; available {2} - Update Date Delta: {3} Days!" .format(ips_date_last_install_iso, ips_current_ver_info, ips_update_ver_info,ips_update_date_delta.days))
-                raise SystemExit, CRITICAL
+                raise SystemExit("CRITICAL")
             else:
                 if ips_update_date_delta.days < 0:
                     print("WARNING! Updates available -  Last installed update: {0} - last Installed {1}; available {2}" .format(ips_date_last_install_iso, ips_current_ver_info, ips_update_ver_info))
-                    raise SystemExit, WARNING
+                    raise SystemExit("WARNING")
                 else:
                     print("WARNING! Updates available -  Last installed update: {0} - last Installed {1}; available {2} - Update Date Delta: {3} Days" .format(ips_date_last_install_iso, ips_current_ver_info, ips_update_ver_info,ips_update_date_delta.days))
-                    raise SystemExit, WARNING    
+                    raise SystemExit("WARNING")    
         else:
             print("meh - something went wrong")
             
