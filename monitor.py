2e7b5884742bceaadf5b139c59a5bc2b2165a002
Daniel Meier <danielsspam@wichtig.ms>
2020-08-30 01:26:13 +0200

    Added SmartCloud1, Fingerprintcheck disabled

--- monitor.py
+++ monitor.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
 #
 # monitor.py
 # version 1.0
 #
 # written by Daniel Meier (github.com/leinadred)
 # July 2018 - updated references to recent cpapi June 2019
-#### Handle with care ####
+# Edit Aug 2020 
+# updated references 
+# added "context" as argument to check for Smart1 Cloud instances
+# corrected States/Exit Codes(Unknown" is "3" now)
+# added to "client_args" - unsafe=True - for not having to handle fingerprints and certificates, as monitoring is not done manually ("do you accept fingerprint xyz?")
+# 
+### Handle with care ####
 #### 
 #### Changed in SDK (to unsecure - no warnings)
 #### mgmt_api.py - line 35 to unsafe_auto_accept=True<---
@@ -25,17 +32,17 @@ parser.add_argument('-C', '--api_context', help='If SmartCloud-1 is used, enter 
 args = parser.parse_args()
 
 # CONSTANTS FOR RETURN CODES UNDERSTOOD BY NAGIOS
-UNKNOWN = -1
 OK = 0
 WARNING = 1
 CRITICAL = 2
+UNKNOWN = 3
 
 # IMPORT Checkpoint SDK
 from cpapi import APIClient, APIClientArgs
 
 
 def main():
-    client_args = APIClientArgs(server=args.api_server, context=args.api_context)
+    client_args = APIClientArgs(server=args.api_server, context=args.api_context, unsafe='True')
     with APIClient(client_args) as client:
         # If Errer occurs due to fingerprtnt mismatch
         if client.check_fingerprint() is False:
@@ -74,7 +81,7 @@ def main():
             #work with it
             if not ips_bool_update:
                 print("OK! No Update available - Last installed update: {0} - Installed Version {1}" .format(ips_date_last_install_iso, ips_current_ver_info))
-                raise SystemExit("OK")
+                raise SystemExit(OK)
             elif ips_update_date_delta.days > 3:
                 print("CRITICAL! Updates available -  Last installed update: {0} - last Installed {1}; available {2} - Update Date Delta: {3} Days!" .format(ips_date_last_install_iso, ips_current_ver_info, ips_update_ver_info,ips_update_date_delta.days))
                 raise SystemExit("CRITICAL")
